9416d5b71d4b8fdc37c443246171151e5ea2c159
Thiago Bandeira <thiago@lar.ifce.edu.br>
2024-08-01 19:49:55 -0300

    feature: foi adicionado a rota de buscar tarefas por id

--- src/main/java/tasks/TaskController.java
+++ src/main/java/tasks/TaskController.java
@@ -1,10 +1,8 @@
 package tasks;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @RestController
@@ -50,4 +48,44 @@ public class TaskController {
             LocalDateTime updated_at
     ) {
     }
+
+    @GetMapping("{task_id}")
+    public GetTaskByIdOutput getById(@PathVariable Long task_id) {
+
+        return new GetTaskByIdOutput(
+                task_id,
+                "Implementar autenticação",
+                "Implementar sistema de login e registro de usuários. ",
+                "a fazer",
+                LocalDate.of(2024, 7, 20),
+                1L,
+                1L,
+                LocalDateTime.of(2024, 7, 17, 0, 0, 0),
+                LocalDateTime.of(2024, 7, 17, 0, 0, 0)
+        );
+
+    }
+
+    public record GetTaskByIdOutput(
+            Long id,
+            String title,
+            String description,
+            String status,
+            LocalDate due_date,
+            Long assigned_id,
+            Long project_id,
+            LocalDateTime created_at,
+            LocalDateTime updated_at
+    ) {
+
+    }
 }
+
+
+
+
+
+
+
+
+
